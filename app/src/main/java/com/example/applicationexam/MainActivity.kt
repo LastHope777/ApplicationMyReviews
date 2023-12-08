a356a42ed216a2f6d01d087e7caee2efe0354f0d
LastHope <nikita_pishk@rambler.ru>
2023-12-08 15:13:15 +0400

    Первый коммит

---
+++ app/src/main/java/com/example/applicationexam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.applicationexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
